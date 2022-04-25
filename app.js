f8ee677a09a0dcffb5385532d24ea6fb336f61cf
johinner <johimadacor@live.com>
2022-04-24 19:21:07 -0500

    cambio de stylesheet css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const puerto = process.env.PORT || 2000;
+const puerto = process.env.PORT || 3000;
 
 //motor de plantillas
 app.set("view engine", "ejs");
