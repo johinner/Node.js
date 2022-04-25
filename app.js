3292635f869b220ff6a11b5798c9d11bce5c9e71
johinner <johimadacor@live.com>
2022-04-24 19:03:20 -0500

    cambio de puerto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const puerto = 2000;
+const puerto = process.env.PORT || 2000;
 
 //motor de plantillas
 app.set("view engine", "ejs");
